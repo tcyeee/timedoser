83956d213be3fa1ef28b7acbfa45cf3c5490290e
tcyeee <tcyeee@outlook.com>
2019-08-21 08:51:01 +0800

    页面更新

--- pages/todoList/todoList.js
+++ pages/todoList/todoList.js
@@ -1,41 +1,75 @@
 import common from '../../common.js';
 
-Page({
-    data: {},
-    toStart() {
-        wx.navigateTo({
-            url: "/pages/timeDoser/timeDoser?minute=" + this.data.minute + "&logName=" + this.data.logName
-        });
-    }
-});
+Page({});
 
 Component({
     data: {
+        index: null,
+        picker: ['喵喵喵', '汪汪汪', '哼唧哼唧'],
         minute: 44,
         logName: "默认番茄",
         allTask: {}
     },
     created: function () {
         console.log("开始获取任务列表");
-        wx.request({
-            url: `${common.URL}/planTask/getAllTask`,
-            header: common.HEADER,
-            method: "POST",
-            success: data => {
-                if (data.statusCode === 200 && data.data.code === '000') {
-                    console.log("allTask请求成功....");
-                    console.log(data.data.data)
-                }
-            }
-        });
-        console.log("获取任务列表结束");
+        getAllTask(this)
     },
     methods: {
         toStart() {
             wx.navigateTo({
                 url: "/pages/timeDoser/timeDoser?minute=" + this.data.minute + "&logName=" + this.data.logName
             });
-        }
+        },
+        addOne(data) {
+            console.log("添加了一个任务");
+            console.log(data.detail.value);
 
+            wx.request({
+                url: `${common.URL}/planTask/addOne`,
+                header: common.HEADER,
+                method: "POST",
+                data: {
+                    planTask: JSON.stringify({
+                        name: data.detail.value.name || "默认番茄",
+                        tomatoWorkTime: parseInt(data.detail.value.minute)
+                    })
+                },
+                success: data => {
+                    if (data.statusCode === 200 && data.data.code === '000') {
+                        getAllTask(this);
+                    }
+                }
+            });
+            this.hideModal();
+        },
+        showModal(e) {
+            this.setData({
+                showModal: true
+            })
+        },
+        hideModal(e) {
+            this.setData({
+                showModal: false
+            })
+        },
+        PickerChange(e) {
+            this.setData({
+                index: e.detail.value
+            })
+        },
     }
+
 });
+
+function getAllTask(that) {
+    wx.request({
+        url: `${common.URL}/planTask/getAllTask`,
+        header: common.HEADER,
+        method: "POST",
+        success: data => {
+            if (data.statusCode === 200 && data.data.code === '000') {
+                that.setData({allTask: data.data.data});
+            }
+        }
+    });
+}
