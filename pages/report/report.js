472f97e493a1e0c07db77ee7215f6b7f1bfef1d8
tcyeee <tcyeee@outlook.com>
2019-09-22 22:52:46 +0800

    添加统计动画

--- pages/report/report.js
+++ pages/report/report.js
@@ -6,30 +6,59 @@ const app = getApp();
 Page({});
 
 Component({
+    data: {
+        dayCount: 0,
+        weekCount: 0,
+        allCount: 0
+    },
     created() {
-
         // follow the page to load base data
         loadBaseData(this);
-        console.log("show time")
     }
 });
-
 
 // load base data
 function loadBaseData(that) {
     wx.request({
-        url: `${common.URL}/planTaskHistory/findAll`,
-        header: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'X_Auth_Token': app.globalData.token
-        },
+        url: `${common.URL}/report/pageData`,
+        header: {'X_Auth_Token': app.globalData.token},
         success: data => {
             if (data.statusCode === 200 && data.data.code === '000') {
-                that.setData({
-                    planHistory: data.data.data
-                });
-                console.log(data.data)
+                that.setData({pageData: data.data.data});
+                // 数字增长效果
+                addNum(that, data.data.data);
             }
         },
+    });
+}
+
+// 数字增长效果
+function addNum(that, data) {
+    setTime(that, 0, data.dayCount, data.weekCount, data.allCount);
+}
+
+// 递增
+function setTime(that, num, day, week, all) {
+    that.setData({
+        dayCount: day < 10 ? 0 : num,
+        weekCount: week < 10 ? 0 : num,
+        allCount: all < 10 ? 0 : num
+    });
+    num++;
+    if (num <= 25) {
+        setTimeout(function () {
+            setTime(that, num, day, week, all);
+        }, 20);
+    } else {
+        setTimeEnd(that, day, week, all);
+    }
+}
+
+// 截止
+function setTimeEnd(that, day, week, all) {
+    that.setData({
+        dayCount: day,
+        weekCount: week,
+        allCount: all
     });
 }
