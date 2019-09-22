399d0ce5351b6c6f37bf880635ed087425403a96
tcyeee <tcyeee@outlook.com>
2019-09-23 00:20:39 +0800

    枚举修改

--- src/main/java/demo/tcyeee/entity/po/BaseUser.java
+++ src/main/java/demo/tcyeee/entity/po/BaseUser.java
@@ -88,8 +88,8 @@ public class BaseUser {
         err(0, "弃用位置"),
         defult(1, "小程序用户, 只有openid, 没有注册"),
         two(2, "手机号注册用户"),
-        author(99, "作者账户"),
-        Administrator(98, "管理员账户");
+        author(3, "作者账户"),
+        Administrator(4, "管理员账户");
 
         private int index;
         private String remark;
