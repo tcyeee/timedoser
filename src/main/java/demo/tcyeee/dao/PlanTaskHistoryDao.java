02968280a936c7571786891a813eb105b157cdc6
huxiong <huxiong@utry.cn>
2019-12-20 16:02:13 +0800

    修复计时bug

--- src/main/java/demo/tcyeee/dao/PlanTaskHistoryDao.java
+++ src/main/java/demo/tcyeee/dao/PlanTaskHistoryDao.java
@@ -14,17 +14,4 @@ import java.util.List;
 @Repository
 public interface PlanTaskHistoryDao extends JpaRepository<PlanTaskHistory, Integer> {
 
-    @Query(nativeQuery = true, value =
-            "select (select sum(tomato_work_time) " +
-                    "        from time_doser.plan_task_history " +
-                    "        where base_user_id = ?1 " +
-                    "          and date(creat_time) = current_date)                                            as day, " +
-                    "       (select sum(tomato_work_time) " +
-                    "        from time_doser.plan_task_history " +
-                    "        where base_user_id = ?1 " +
-                    "          and date_format(creat_time, '%Y-%m') = date_format(current_timestamp, '%Y-%m')) as week, " +
-                    "       sum(plan_task_id)                                                                  as allSum " +
-                    "from time_doser.plan_task_history " +
-                    "where base_user_id = ?1")
-    List<Object[]> diyPageData(String id);
 }
--- src/main/java/demo/tcyeee/service/impl/ReportServiceImpl.java
+++ src/main/java/demo/tcyeee/service/impl/ReportServiceImpl.java
@@ -1,15 +1,13 @@
 package demo.tcyeee.service.impl;
 
-import demo.tcyeee.dao.PlanTaskHistoryDao;
 import demo.tcyeee.entity.po.BaseUser;
 import demo.tcyeee.entity.vo.ReportPageDataVo;
+import demo.tcyeee.mapper.PlanTaskHistoryMapper;
 import demo.tcyeee.service.ReportService;
 import demo.tcyeee.utils.BaseUtils;
-import demo.tcyeee.utils.EntityUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * @author chenyueee
@@ -17,9 +15,8 @@ import java.util.List;
  */
 @Service
 public class ReportServiceImpl implements ReportService {
-
     @Resource
-    private PlanTaskHistoryDao planTaskHistoryDao;
+    private PlanTaskHistoryMapper planTaskHistoryMapper;
 
     /**
      * report页面初始化需要的数据
@@ -29,7 +26,6 @@ public class ReportServiceImpl implements ReportService {
     @Override
     public ReportPageDataVo pageData() {
         BaseUser baseUser = BaseUtils.userInfo();
-        List<Object[]> objects = planTaskHistoryDao.diyPageData(baseUser.getId());
-        return EntityUtils.castEntity(objects, ReportPageDataVo.class).get(0);
+        return planTaskHistoryMapper.pageData(baseUser.getId());
     }
 }
