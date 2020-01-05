612fa4e2f8490cd1308761cd0a54dcea4c61d33e
tcyeee <tcyeee@outlook.com>
2020-01-05 23:26:22 +0800

    个人主页改版

--- pages/my/home/home.js
+++ pages/my/home/home.js
@@ -2,10 +2,20 @@ import common from "../../../common.js";
 
 const app = getApp();
 
-Page({});
+Page({
+    created() {
+        loadBaseData(this);
+    },
+});
 Component({
     data: {
-        baseUser: wx.getStorageSync("baseUser")
+        baseUser: wx.getStorageSync("baseUser"),
+        dayCount: 0,
+        weekCount: 0,
+        allCount: 0
+    },
+    attached: function () {
+        loadBaseData(this);
     },
     methods: {
         getUserInfo: function (e) {
@@ -56,4 +66,51 @@ function checkData(that) {
     if (that.data.baseUser === null || that.data.baseUser === "" || that.data.baseUser.avatarUrl === null || that.data.baseUser.avatarUrl === "") {
         that.setData({baseUser: wx.getStorageSync("baseUser")});
     }
+}
+
+
+// load base data
+function loadBaseData(that) {
+    wx.request({
+        url: `${common.URL}/report/pageData`,
+        header: {'X_Auth_Token': app.globalData.token},
+        success: data => {
+            if (data.statusCode === 200 && data.data.code === 200) {
+                that.setData({pageData: data.data.data});
+                // 数字增长效果
+                addNum(that, data.data.data);
+            }
+        },
+    });
+}
+
+// 数字增长效果
+function addNum(that, data) {
+    setTime(that, 0, data.dayCount, data.weekCount, data.allCount);
+}
+
+// 递增
+function setTime(that, num, day, week, all) {
+    that.setData({
+        dayCount: day === undefined || day < 10 ? 0 : num,
+        weekCount: week === undefined || week < 10 ? 0 : num,
+        allCount: all === undefined || all < 10 ? 0 : num
+    });
+    num++;
+    if (num <= 25) {
+        setTimeout(function () {
+            setTime(that, num, day, week, all);
+        }, 20);
+    } else {
+        setTimeEnd(that, day, week, all);
+    }
+}
+
+// 截止
+function setTimeEnd(that, day, week, all) {
+    that.setData({
+        dayCount: day === undefined ? 0 : day,
+        weekCount: week === undefined ? 0 : week,
+        allCount: all === undefined ? 0 : all
+    });
 }
