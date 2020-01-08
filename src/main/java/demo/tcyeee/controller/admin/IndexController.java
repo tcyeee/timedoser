491404be665d333e2f8d92a9d0aa7773ebcb327e
huxiong <huxiong@utry.cn>
2020-01-08 10:13:40 +0800

    更新系统细节

--- src/main/java/demo/tcyeee/controller/admin/IndexController.java
+++ src/main/java/demo/tcyeee/controller/admin/IndexController.java
@@ -2,6 +2,7 @@ package demo.tcyeee.controller.admin;
 
 import demo.tcyeee.service.AdminIndexService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -12,7 +13,8 @@ import static demo.tcyeee.utils.ResponseUtils.creatJsonResponse;
  * @author huxiong
  * @date 2020/1/7 16:52
  */
-@RestController("adminIndex")
+@RestController
+@RequestMapping("adminIndex")
 public class IndexController {
 
     @Resource
