d7a9107a6e6a76f0a8670a6e0fae18606e3e48a2
huxiong <huxiong@utry.cn>
2020-03-24 18:12:54 +0800

    更新

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,6 @@
 import common from '../../common.js';
 
+const util = require('../../utils/util.js');
 const app = getApp();
 
 Page({
@@ -12,184 +13,30 @@ Page({
         changedue: false,      // 是否除于动画过程
         checkProject: false,   // 是否点击项目
         createProject: false,  // 创建项目窗口
-        timeList: [{
-            title: '15',
-            name: 'orange',
-        }, {
-            title: '25',
-            name: 'yellow',
-        }, {
-            title: '45',
-            name: 'olive',
-        }, {
-            title: '60',
-            name: 'green',
-        }, {
-            title: '90',
-            name: 'cyan',
-        }, {
-            title: '120',
-            name: 'blue',
-        }],
-        color1: [
-            {inex: 0, color: 'red'},
-            {inex: 1, color: 'orange'},
-            {inex: 2, color: 'yellow'},
-            {inex: 3, color: 'olive'},
-            {inex: 4, color: 'green'},
-            {inex: 5, color: 'cyan'},
-            {inex: 6, color: 'blue'},
-            {inex: 7, color: 'purple'},
-            {inex: 8, color: 'mauve'},
-            {inex: 9, color: 'pink'},
-            {inex: 10, color: 'brown'},
-            {inex: 12, color: 'grey'},
-        ],
-        color2: [
-            {index: 0, color: 'redLight'},
-            {index: 1, color: 'orangeLight'},
-            {index: 2, color: 'yellowLight'},
-            {index: 3, color: 'oliveLight'},
-            {index: 4, color: 'greenLight'},
-            {index: 5, color: 'cyanLight'},
-            {index: 6, color: 'blueLight'},
-            {index: 7, color: 'purpleLight'},
-            {index: 8, color: 'mauveLight'},
-            {index: 9, color: 'pinkLight'},
-            {index: 10, color: 'brownLight'},
-            {index: 11, color: 'greyLight'},
-        ],
-        icon: [
-            {index: 0, icon: 'appreciate'},
-            {index: 1, icon: 'emoji'},
-            {index: 2, icon: 'favor'},
-            {index: 3, icon: 'loading'},
-            {index: 4, icon: 'roundcheck'},
-            {index: 5, icon: 'search'},
-            {index: 6, icon: 'time'},
-            {index: 7, icon: 'warn'},
-            {index: 8, icon: 'camera'},
-            {index: 9, icon: 'like'},
-            {index: 10, icon: 'deliver'},
-            {index: 11, icon: 'evaluate'},
-            {index: 12, icon: 'send'},
-            {index: 13, icon: 'shop'},
-            {index: 14, icon: 'ticket'},
-            {index: 15, icon: 'discover'},
-            {index: 16, icon: 'footprint'},
-            {index: 17, icon: 'cart'},
-            {index: 18, icon: 'remind'},
-            {index: 19, icon: 'lock'},
-            {index: 20, icon: 'goods'},
-            {index: 21, icon: 'selection'},
-            {index: 22, icon: 'explore'},
-            {index: 23, icon: 'present'},
-            {index: 24, icon: 'round'},
-            {index: 25, icon: 'game'},
-            {index: 26, icon: 'vipcard'},
-            {index: 27, icon: 'light'},
-            {index: 28, icon: 'notice'},
-            {index: 29, icon: 'upstage'},
-            {index: 30, icon: 'baby'},
-            {index: 31, icon: 'clothes'},
-            {index: 32, icon: 'creative'},
-            {index: 33, icon: 'female'},
-            {index: 34, icon: 'male'},
-            {index: 35, icon: 'rank'},
-            {index: 36, icon: 'bad'},
-            {index: 37, icon: 'cameraadd'},
-            {index: 38, icon: 'focus'},
-            {index: 39, icon: 'file'},
-            {index: 40, icon: 'attention'},
-            {index: 41, icon: 'read'},
-            {index: 42, icon: 'magic'},
-            {index: 43, icon: 'tag'},
-            {index: 44, icon: 'all'},
-            {index: 45, icon: 'write'},
-            {index: 46, icon: 'crown'},
-            {index: 47, icon: 'musicfill'},
-            {index: 48, icon: 'record'},
-            {index: 49, icon: 'cardboard'},
-            {index: 50, icon: 'mail'},
-            {index: 51, icon: 'goodsnew'},
-            {index: 52, icon: 'medal'},
-            {index: 53, icon: 'newshot'},
-            {index: 54, icon: 'news'},
-            {index: 55, icon: 'skin'},
-            {index: 56, icon: 'usefull'},
-        ],
+        timeList: [],
+        color1: [],
+        color2: [],
+        icon: [],
+        projectList: [],
+        projectListLimit: {},
+        project: {},
+        nowMin: 25,                // 现在的时间
+        nowUpdataProjectId: null,  // 现在修改的项目ID
+        exitInfo: false,           // 退出警告
         newProjectInfo: {
+            id: '',
             name: '',
             remark: '',
             icon: '',
             color: ''
         },
-        projectList: [{
-            id: 1,
-            name: '小程序研发',
-            remark: '制作一个番茄时钟软件,作为自己的面试作品,也为了能够更好的使用小程序',
-            icon: 'like',
-            color: 'pink',
-            score: 996
-        }, {
-            id: 2,
-            name: '英语学习',
-            remark: '滴滴滴滴这是一个学习序',
-            icon: 'mail',
-            color: 'green',
-            score: 39
-        }, {
-            id: 3,
-            name: '考研',
-            remark: '好好学习好好考研',
-            icon: 'vipcard',
-            color: 'cyan',
-            score: 50
-        }, {
-            id: 4,
-            name: 'UI学习',
-            remark: '学一下好用的东西',
-            icon: 'magic',
-            color: 'purple',
-            score: 25
-        }],
-        projectListLimit: [{
-            id: 1,
-            name: '小程序研发',
-            remark: '制作一个番茄时钟软件,作为自己的面试作品,也为了能够更好的使用小程序',
-            icon: 'like',
-            color: 'pink',
-            score: 996
-        }, {
-            id: 2,
-            name: '英语学习',
-            remark: '滴滴滴滴这是一个学习序',
-            icon: 'mail',
-            color: 'green',
-            score: 39
-        }, {
-            id: 3,
-            name: '考研',
-            remark: '好好学习好好考研',
-            icon: 'vipcard',
-            color: 'cyan',
-            score: 50
-        }],
-        project: {
-            name: '小程序研发',
-            remark: '制作一个番茄时钟软件,作为自己的面试作品,也为了能够更好的使用小程序',
-            icon: 'like',
-            color: 'pink',
-            sumMinute: 996
-        },
-        nowMin: 25,   // 现在的时间
-        nowUpdataProjectId: null,  // 现在修改的项目ID
-        exitInfo: false,    // 退出警告
-
     },
     onLoad() {
         // 设置初始透明度
         // setRemarkOpecity(this);
+
+        // 设定初始数据
+        setDefaultData(this);
 
         // 获取所有的项目信息
         findAll(this);
@@ -261,6 +108,7 @@ Page({
 
     // 随机更新一种颜色
     randomIcon() {
+        console.log(util.wxuuid());
         const iconList = this.data.icon;
         const iconIndex = parseInt(Math.random() * iconList.length);
         const icon = iconList[iconIndex].icon;
@@ -314,7 +162,33 @@ Page({
         ], 100);
     },
     create(e) {
-        console.log(this.data.newProjectInfo)
+        this.createOneClose();
+        const project = this.data.newProjectInfo;
+
+        // create a project id at random
+        project.id = util.wxuuid();
+
+        const projectList = this.data.projectList;
+        projectList.push(project);
+
+        this.setData({
+            projectList: projectList,
+            nowUpdataProjectId: null
+        });
+
+        wx.request({
+            url: `${common.URL}/project/save`,
+            header: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'X_Auth_Token': app.globalData.token
+            },
+            method: "post",
+            data: project,
+            success: res => {
+                findAll(this);
+            }
+        });
+
     },
     setProjectRemark(e) {
         this.data.newProjectInfo.remark = e.detail.value;
@@ -339,6 +213,27 @@ Page({
     },
     updateOneClose() {
         this.setData({nowUpdataProjectId: null});
+    },
+
+    // 删除项目
+    projectDelete(index) {
+        const projectId = index.target.dataset.index;
+
+
+        wx.request({
+            url: `${common.URL}/project/deleteOne`,
+            header: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'X_Auth_Token': app.globalData.token
+
+            },
+            method: "get",
+            data: {"id": projectId},
+            success: res => {
+                console.log(res);
+                findAll(this);
+            }
+        });
     },
 
     // 进入设置页面
@@ -442,7 +337,6 @@ function findAll(that) {
             });
         }
 
-
         // 关掉首页的提示窗口
         wx.hideToast();
 
@@ -456,7 +350,6 @@ function findAll(that) {
             method: "get",
             success: requsData => {
                 if (requsData.statusCode === 200 && requsData.data.code === 200) {
-                    console.log(requsData.data.data);
 
                     // 设置最近一次项目
                     requsData.data.data.forEach(function (item) {
@@ -480,4 +373,171 @@ function findAll(that) {
             }
         });
     }
+}
+
+
+function setDefaultData(that) {
+
+    const timeList = [
+        {
+            title: '15',
+            name: 'orange',
+        }, {
+            title: '25',
+            name: 'yellow',
+        }, {
+            title: '45',
+            name: 'olive',
+        }, {
+            title: '60',
+            name: 'green',
+        }, {
+            title: '90',
+            name: 'cyan',
+        }, {
+            title: '120',
+            name: 'blue',
+        }
+    ];
+
+    const color1 = [
+        {inex: 0, color: 'red'},
+        {inex: 1, color: 'orange'},
+        {inex: 2, color: 'yellow'},
+        {inex: 3, color: 'olive'},
+        {inex: 4, color: 'green'},
+        {inex: 5, color: 'cyan'},
+        {inex: 6, color: 'blue'},
+        {inex: 7, color: 'purple'},
+        {inex: 8, color: 'mauve'},
+        {inex: 9, color: 'pink'},
+        {inex: 10, color: 'brown'},
+        {inex: 12, color: 'grey'}
+    ];
+
+    const color2 = [
+        {index: 0, color: 'redLight'},
+        {index: 1, color: 'orangeLight'},
+        {index: 2, color: 'yellowLight'},
+        {index: 3, color: 'oliveLight'},
+        {index: 4, color: 'greenLight'},
+        {index: 5, color: 'cyanLight'},
+        {index: 6, color: 'blueLight'},
+        {index: 7, color: 'purpleLight'},
+        {index: 8, color: 'mauveLight'},
+        {index: 9, color: 'pinkLight'},
+        {index: 10, color: 'brownLight'},
+        {index: 11, color: 'greyLight'}
+    ];
+
+    const icon = [
+        {index: 0, icon: 'appreciate'},
+        {index: 1, icon: 'emoji'},
+        {index: 2, icon: 'favor'},
+        {index: 3, icon: 'loading'},
+        {index: 4, icon: 'roundcheck'},
+        {index: 5, icon: 'search'},
+        {index: 6, icon: 'time'},
+        {index: 7, icon: 'warn'},
+        {index: 8, icon: 'camera'},
+        {index: 9, icon: 'like'},
+        {index: 10, icon: 'deliver'},
+        {index: 11, icon: 'evaluate'},
+        {index: 12, icon: 'send'},
+        {index: 13, icon: 'shop'},
+        {index: 14, icon: 'ticket'},
+        {index: 15, icon: 'discover'},
+        {index: 16, icon: 'footprint'},
+        {index: 17, icon: 'cart'},
+        {index: 18, icon: 'remind'},
+        {index: 19, icon: 'lock'},
+        {index: 20, icon: 'goods'},
+        {index: 21, icon: 'selection'},
+        {index: 22, icon: 'explore'},
+        {index: 23, icon: 'present'},
+        {index: 24, icon: 'round'},
+        {index: 25, icon: 'game'},
+        {index: 26, icon: 'vipcard'},
+        {index: 27, icon: 'light'},
+        {index: 28, icon: 'notice'},
+        {index: 29, icon: 'upstage'},
+        {index: 30, icon: 'baby'},
+        {index: 31, icon: 'clothes'},
+        {index: 32, icon: 'creative'},
+        {index: 33, icon: 'female'},
+        {index: 34, icon: 'male'},
+        {index: 35, icon: 'rank'},
+        {index: 36, icon: 'bad'},
+        {index: 37, icon: 'cameraadd'},
+        {index: 38, icon: 'focus'},
+        {index: 39, icon: 'file'},
+        {index: 40, icon: 'attention'},
+        {index: 41, icon: 'read'},
+        {index: 42, icon: 'magic'},
+        {index: 43, icon: 'tag'},
+        {index: 44, icon: 'all'},
+        {index: 45, icon: 'write'},
+        {index: 46, icon: 'crown'},
+        {index: 47, icon: 'musicfill'},
+        {index: 48, icon: 'record'},
+        {index: 49, icon: 'cardboard'},
+        {index: 50, icon: 'mail'},
+        {index: 51, icon: 'goodsnew'},
+        {index: 52, icon: 'medal'},
+        {index: 53, icon: 'newshot'},
+        {index: 54, icon: 'news'},
+        {index: 55, icon: 'skin'},
+        {index: 56, icon: 'usefull'}
+    ];
+
+    const projectList = [{
+        id: 1,
+        name: '小程序研发',
+        remark: '制作一个番茄时钟软件,作为自己的面试作品,也为了能够更好的使用小程序',
+        icon: 'like',
+        color: 'pink',
+        score: 996
+    }, {
+        id: 2,
+        name: '英语学习',
+        remark: '滴滴滴滴这是一个学习序',
+        icon: 'mail',
+        color: 'green',
+        score: 39
+    }, {
+        id: 3,
+        name: '考研',
+        remark: '好好学习好好考研',
+        icon: 'vipcard',
+        color: 'cyan',
+        score: 50
+    }, {
+        id: 4,
+        name: 'UI学习',
+        remark: '学一下好用的东西',
+        icon: 'magic',
+        color: 'purple',
+        score: 25
+    }];
+
+    let projectListLimit = [];
+    let project = {};
+    projectList.forEach(function (item) {
+        if (item.lastProject) {
+            project = item;
+        }
+        if (item.length < 3) {
+            projectListLimit.push(item)
+        }
+    });
+
+    that.setData({
+        projectList: projectList,
+        timeList: timeList,
+        color1: color1,
+        color2: color2,
+        icon: icon,
+        project: project,
+        projectListLimit: projectListLimit
+    })
 }
