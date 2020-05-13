1a31c0839105a0dc4c20c963dd91915f16a2c819
tcyeee <tcyeee@outlook.com>
2020-05-13 20:50:20 +0800

    新增项目

---
+++ microservice-user/src/test/java/com/timedoser/cloud/microserviceuser/MicroserviceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timedoser.cloud.microserviceuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
