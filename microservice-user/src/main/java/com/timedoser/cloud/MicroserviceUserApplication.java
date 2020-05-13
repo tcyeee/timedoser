cf7c9ba1821b04e88752769fd5b61a006ab5e178
tcyeee <tcyeee@outlook.com>
2020-05-13 21:01:14 +0800

    add dependency

--- microservice-user/src/main/java/com/timedoser/cloud/MicroserviceUserApplication.java
+++ microservice-user/src/main/java/com/timedoser/cloud/MicroserviceUserApplication.java
@@ -1,4 +1,4 @@
-package com.timedoser.cloud.microserviceuser;
+package com.timedoser.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
