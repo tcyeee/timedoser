c38f554edd93f6c94ffe560311b1b1e1f47d2df6
tcyeee <tcyeee@outlook.com>
2020-05-30 20:15:43 +0800

    修改文件夹命名

---
+++ module-main/src/main/java/com/timedoser/cloud/maim/ModuleMaimApplication.java
@@ -0,0 +1,13 @@
+package com.timedoser.cloud.maim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleMaimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleMaimApplication.class, args);
+    }
+
+}
---
+++ module-main/src/test/java/com/timedoser/cloud/maim/ModuleMaimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timedoser.cloud.maim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleMaimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
