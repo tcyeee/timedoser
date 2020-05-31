ecaac3b762cc32d64433623d608a2635f3f9f688
tcyeee <tcyeee@outlook.com>
2020-05-31 18:10:36 +0800

    可以兼容的版本

--- module-main/src/main/java/com/timedoser/cloud/main/MainApplication.java
+++ module-main/src/main/java/com/timedoser/cloud/main/MainApplication.java
@@ -2,7 +2,9 @@ package com.timedoser.cloud.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
