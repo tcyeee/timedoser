f82f1202eac9f5b9f8068ffab43dfa0e0c66b034
tcyeee <tcyeee@outlook.com>
2020-05-31 21:13:14 +0800

    添加全局编码/json

--- module-user/src/main/java/com/timedoser/cloud/user/UserApplication.java
+++ module-user/src/main/java/com/timedoser/cloud/user/UserApplication.java
@@ -3,9 +3,11 @@ package com.timedoser.cloud.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.timedoser.cloud"})
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
