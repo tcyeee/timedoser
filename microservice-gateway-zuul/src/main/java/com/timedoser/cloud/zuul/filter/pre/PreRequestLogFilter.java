ca420e5953c2a4b9bde4f7d6425a26b7534864dc
tcyeee <tcyeee@outlook.com>
2020-06-01 22:21:13 +0800

    添加zuul过滤器

---
+++ microservice-gateway-zuul/src/main/java/com/timedoser/cloud/zuul/filter/pre/PreRequestLogFilter.java
@@ -0,0 +1,40 @@
+package com.timedoser.cloud.zuul.filter.pre;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author tcyeee
+ * @since 2020/6/1 10:07 下午
+ */
+public class PreRequestLogFilter extends ZuulFilter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PreRequestLogFilter.class);
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        PreRequestLogFilter.LOGGER.info(String.format("send %s request to %s", request.getMethod(), request.getRequestURL().toString()));
+        return null;
+    }
+}
