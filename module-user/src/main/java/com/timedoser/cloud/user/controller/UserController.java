0d4e12042665a4976a2d62f24f19aeb8e42da684
huxiong <huxiong@utry.cn>
2020-06-09 13:25:14 +0800

    添加mybatis plus

--- module-user/src/main/java/com/timedoser/cloud/user/controller/UserController.java
+++ module-user/src/main/java/com/timedoser/cloud/user/controller/UserController.java
@@ -20,18 +20,6 @@ public class UserController {
     @Resource
     private UserService userService;
 
-//    /**
-//     * 小程序用户通过授权以后,更新数据库中基础用户数据
-//     *
-//     * @return data
-//     */
-//    @RequestMapping("updateUserInfo")
-//    public String updateUserInfo(String userInfo) {
-//        if (StringUtils.isBlank(userInfo) || "null".equals(userInfo)) return creatErrResponse(PARAMS_ERROR_INFO);
-//        return creatJsonResponse(userService.updataUserInfo(userInfo));
-//    }
-
-
     /**
      * 通过手机号查看用户信息
      *
