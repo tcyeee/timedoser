d9d617d8d27715667524139a236370552d618ab4
huxiong <huxiong@utry.cn>
2020-06-18 17:16:58 +0800

    添加格式校验

--- module-main/src/main/java/com/timedoser/cloud/main/common/entity/vo/UserPasswordVo.java
+++ module-main/src/main/java/com/timedoser/cloud/main/common/entity/vo/UserPasswordVo.java
@@ -2,8 +2,6 @@ package com.timedoser.cloud.main.common.entity.vo;
 
 import lombok.Data;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
@@ -16,8 +14,6 @@ public class UserPasswordVo {
 
     @NotBlank(message = "phoneNumber不可为空!")
     @Pattern(regexp = "^[1][3,4,5,6,7,8,9][0-9]{9}$", message = "手机号格式有误")
-    @Max(value = 11, message = "手机号只能为{max}位")
-    @Min(value = 11, message = "手机号只能为{min}位")
     private String phoneNumber;
 
     @NotBlank(message = "password不可为空!")
