49aff2208a39edc300f14c0c3cfec3cd181e02fa
tcyeee <tcyeee@outlook.com>
2020-06-21 13:00:04 +0800

    添加跨域处理

---
+++ microservice-gateway-zuul/src/main/java/com/timedoser/cloud/zuul/config/CrossOriginConfig.java
@@ -0,0 +1,21 @@
+package com.timedoser.cloud.zuul.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 添加跨域解决处理
+ *
+ * @author tcyeee
+ * @date 2020/6/21 12:34 下午
+ */
+@Configuration
+public class CrossOriginConfig implements WebMvcConfigurer {
+    static final String[] ORIGINS = new String[]{"GET", "POST", "PUT", "DELETE"};
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowCredentials(true).allowedMethods(ORIGINS).maxAge(3600);
+    }
+}
